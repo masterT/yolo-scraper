c2257724f10dc1963f7603c7152b28cc51b394e8
Joe Strong <joestrong@gmail.com>
2018-10-18 21:02:45 +0100

    Adds linter and reformats code

--- examples/usingParamsSchema.js
+++ examples/usingParamsSchema.js
@@ -1,54 +1,53 @@
-var yoloScraper = require('../lib/index.js');
-
+var yoloScraper = require('../lib/index.js')
 
 var scraper = yoloScraper.createScraper({
 
   paramsSchema: {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "type": "string",
-    "minLength": 1
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'string',
+    'minLength': 1
   },
 
   request: function (username) {
-    return 'https://www.npmjs.com/~' + username.toLowerCase();
+    return 'https://www.npmjs.com/~' + username.toLowerCase()
   },
 
   extract: function (response, body, $) {
     return $('.collaborated-packages li').toArray().map(function (element) {
-      var $element = $(element);
+      var $element = $(element)
       return {
         name: $element.find('a').text(),
         url: $element.find('a').attr('href'),
         version: $element.find('strong').text()
-      };
-    });
+      }
+    })
   },
 
   schema: {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "type" : "array",
-    "items": {
-      "type": "object",
-      "additionalProperties": false,
-      "properties": {
-        "name": { "type": "string" },
-        "url": { "type": "string", "format": "uri" },
-        "version": { "type": "string", "pattern": "^v\\d+\\.\\d+\\.\\d+$" }
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'array',
+    'items': {
+      'type': 'object',
+      'additionalProperties': false,
+      'properties': {
+        'name': { 'type': 'string' },
+        'url': { 'type': 'string', 'format': 'uri' },
+        'version': { 'type': 'string', 'pattern': '^v\\d+\\.\\d+\\.\\d+$' }
       },
-      "required": [ "name", "url", "version" ]
+      'required': [ 'name', 'url', 'version' ]
     }
   }
 
-});
+})
 
-var validParams = "masterT";
-var invalidParams = "";
+var validParams = 'masterT'
+// var invalidParams = ''
 
 scraper(validParams, function (error, data) {
 // scraper(invalidParams, function (error, data) {
   if (error) {
-    console.log('error:', error);
+    console.log('error:', error)
   } else {
-    console.log('data:', data);
+    console.log('data:', data)
   }
-});
+})
